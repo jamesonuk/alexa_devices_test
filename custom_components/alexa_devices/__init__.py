1c2da7f6a705b122b2b46a305644b8838ccef90e
jamesonuk <1040621+jamesonuk@users.noreply.github.com>
2026-04-19 10:25:59 +0100

    fix stop method name

--- custom_components/alexa_devices/__init__.py
+++ custom_components/alexa_devices/__init__.py
@@ -100,7 +100,7 @@ async def async_migrate_entry(hass: HomeAssistant, entry: AmazonConfigEntry) -> 
 async def async_unload_entry(hass: HomeAssistant, entry: AmazonConfigEntry) -> bool:
     """Unload a config entry."""
     try:
-        await entry.runtime_data.api.stop_http2_thread()
+        await entry.runtime_data.api.stop_http2_processing()
     except Exception:  # noqa: BLE001
-        _LOGGER.error("Error while stopping http2 thread", exc_info=True)
+        _LOGGER.error("Error while stopping http2 processing", exc_info=True)
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
